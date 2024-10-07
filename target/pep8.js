34f5f86249573605ba4630876fc6446280b9edce
Olivier Arteau <arteau.olivier@gmail.com>
2024-10-07 13:06:01 -0400

    Push updated artifact

--- target/pep8.js
+++ target/pep8.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpo_2snp_2.js
+// include: /tmp/tmpdlri7aef.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -226,21 +226,21 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
   })();
 
-// end include: /tmp/tmpo_2snp_2.js
-// include: /tmp/tmp5w4y3csp.js
+// end include: /tmp/tmpdlri7aef.js
+// include: /tmp/tmpbchfsf00.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp5w4y3csp.js
-// include: /tmp/tmpc2bc75zz.js
+  // end include: /tmp/tmpbchfsf00.js
+// include: /tmp/tmpht9nhrlx.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpc2bc75zz.js
+  // end include: /tmp/tmpht9nhrlx.js
 
 
 // Sometimes an existing Module object exists with properties
